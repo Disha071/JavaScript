dde78c0ab4f0ac41f50e54525aa7b2331ca4803d
Disha071 <disha.gupta_cs22@gla.ac.in>
2024-08-13 16:54:27 +0530

    JavaScript

---
+++ first1.js
@@ -0,0 +1,15 @@
+// document.write('welcome to GLA UNIVERSITY')
+// document.write('<br>')
+// document.write('<br>')
+// document.write('<h2>Branches in GLA</h2>')
+// document.write('<h2>CSE</h2>')
+// document.write('<br>')
+// document.write('<h2>Mechanical</h2>')
+// document.write('<br>')
+// document.write('<h2>Electrical</h2>')
+// document.write('<br>')
+
+
+
+
+
---
+++ user.py
@@ -0,0 +1,12 @@
+# str1=int(input("Enter your nnumber1->"))
+# str2=int(input("Enter your number 2->"))
+# res=str1+str2
+# print('the sum of a is:',res)
+
+# str1=eval(input("Enter your nnumber1->"))
+# str2=eval(input("Enter your number 2->"))
+# res=str1+str2
+# print('the sum of a is:',res)
+
+str1='rahul'
+print(str1.capitalize())
